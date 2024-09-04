c25bb293b2d55cbb9d50a9740202267900002838
Mayaismi <mayaismi21@gmail.com>
2024-09-04 12:06:26 +0700

    Create day3.java

---
+++ Create day3.java
@@ -0,0 +1,6 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("maya");
+        System.out.println("Besoangin utara");
+        System.out.println("Smkn 1 Majene");
+    }

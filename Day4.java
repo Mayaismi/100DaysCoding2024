43bc2d29ceda154249a9e1302c1b6443d0d1f3f3
Mayaismi <mayaismi21@gmail.com>
2024-09-05 09:31:23 +0700

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,6 @@
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.print("Nama saya Maya ");
+        System.out.printf("Prodi %s umur saya %d Tahun","Informatika",18);
+    }
+}

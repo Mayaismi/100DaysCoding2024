f5e9345c85f8854bcc1b4c60c2448308070bfaa6
Mayaismi <mayaismi21@gmail.com>
2024-09-06 10:15:07 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Day5 {
+    public static void main(String[] args) {
+        String nama = "Maya";
+        int umur    =  18;
+        int tinggi  =  148;
+        System.out.printf("Nama  : %s\nUmur  : %d Tahun\nTinggi: %d\n","Maya", 18 ,148);
+        //printf %s/string adalah tipe data yang dimana nilainya di apit oleh 2 tanda petik
+        //%d/int/integer untuk menghasilkan angka
+        System.out.print("Prodi : Informatika\n");
+       //*Print tidak berbaris
+        System.out.println("Fakultas : Teknik");
+        /*println Berbaris*/
+    }

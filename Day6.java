738c22565e190779e22a30e862e57ac8ac910225
Mayaismi <mayaismi21@gmail.com>
2024-09-07 16:12:12 +0700

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,8 @@
+public class Day6 {
+    //Bagian class
+    public static void main(String[] args) {
+        //mentod main
+        String nama;
+        //membuat variabel kosong bertipe data 
+    }
+}

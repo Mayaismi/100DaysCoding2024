bd3577f529fca6398b8b2508cf7d2ac041125836
Mayaismi <mayaismi21@gmail.com>
2024-09-08 12:08:18 +0700

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,25 @@
+public class Day7 {
+    //bagian class
+    public static void main(String[] args) {
+        //medtod main
+        
+        //Membuat variabel
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        
+        //Mengisi variabel
+        nama    ="Maya";
+        alamat  ="Besoangin utara";
+        usia    =   18;
+        tinggi  =   148;
+        
+        //Mencetak ke layar isi variabel 
+        System.out.println("Nama  : " + nama);
+        System.out.println("Alamat:" + alamat);
+        System.out.println("usia  :" + usia + " Tahun");
+        System.out.println("Tinggi:" + tinggi + "cm");
+        
+    }
+}
+

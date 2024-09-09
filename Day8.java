be9cd988b374413162eb02fe08565a356d5273cd
Mayaismi <mayaismi21@gmail.com>
2024-09-09 17:09:14 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,21 @@
+public class Day8 {
+   //Merubah folder yang dalamnya berisi program java
+    public static void main(String[] args) {
+        //Method main
+        String nama = "Besoangin Utara";
+        //Inisialisasi variabel setelah deklarasi
+        System.out.println("Alamat           : " + nama);
+        
+        nama = "Maya";
+        //update nilai Variabel
+        System.out.println("Nama             : "+nama );
+        
+        int tinggi = 148;
+        //Inisialisasi variabel setelah deklarasi
+        System.out.println("Tinggi           : " + tinggi +"cm" );
+        
+        tinggi = 155;
+        //update nilai variabel
+        System.out.println("Mau tinggi segitu: " + tinggi + "cm");
+    }
+}

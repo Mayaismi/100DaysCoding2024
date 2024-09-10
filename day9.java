4752a81732f838d8b709b7ef2a5e62b64d416042
Mayaismi <mayaismi21@gmail.com>
2024-09-10 17:24:54 +0700

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,12 @@
+public class Day9 {
+    public static void main(String[] args) {
+        final String nama = "Maya";
+        final String nim = "D0224041";
+        final String kelas = "A";
+        final String prodi = "Informatika";
+        //Mendeklarasikan variabel yang tidak dapat di ubah/Final dan mengisi masing" variabel
+
+        System.out.println("Nama:" + nama + " Nim:" + nim + " Kelas:"+ kelas + " Prodi:" + prodi);
+        //Menampilkan biodata dalam satu baris
+    }
+}

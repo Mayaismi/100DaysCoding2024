f310a90aa0175e82ee3a3163f2d7bd0b96d38217
Mayaismi <mayaismi21@gmail.com>
2024-09-11 17:48:52 +0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class Day10 {
+    //Class main
+       public static void main(String[] args) {
+        //Mentod main 
+        byte bb = 38;
+        //inisialisasi variabel
+           System.out.println("Berat badan :" + bb);
+           //Mengisi Variabel
+    }
+}

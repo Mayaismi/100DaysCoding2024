138601022c5eb04654e00794263d55693cede324
Mayaismi <mayaismi21@gmail.com>
2024-09-12 17:34:32 +0700

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,12 @@
+public class Day11 {
+    //Bagian class
+    public static void main(String[] args) {
+        //Mentod main
+        short bb = 38;
+        //inisialisasi variabel
+        //short tipe data digunakan untuk menyimpan nilai bilangan bulat yang lebih besar dari tipe data
+        //byte rentang nilai yang dapat disimpan -32,768 sampai 32767
+        System.out.println("Berat badan: "+bb);
+        //Menampilkan hasil Variabel yang sudah di buat 
+    }
+}

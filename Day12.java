5e1749ec2ec51948f0e9c18ed7e7a1a140d98947
Mayaismi <mayaismi21@gmail.com>
2024-09-13 19:43:58 +0700

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,9 @@
+public class Day12 {
+public static void main(String[] args) {
+  int minimun =  -2147483;
+  int maksimun = 2147483;
+  
+  System.out.println("Minimun : " + minimun);
+  System.out.println("Maksimun: " + maksimun);
+    }
+}

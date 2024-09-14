1be82e1b494541e796e2ad77189eac4afc2d196c
Mayaismi <mayaismi21@gmail.com>
2024-09-14 19:44:49 +0700

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,33 @@
+package evaluasi;
+
+
+public class Evaluasi1 {
+    public static void main(String[] args) {
+        System.out.printf("Nama: %s ","Maya");
+        System.out.println("");
+        System.out.printf("Nim: %s","D0224041");
+        System.out.println("");
+        System.out.printf("Umur: %d",18);
+        System.out.println();
+    }
+}
+    //deklarasi hnya menambahkan variabel tidak dengan isinya
+    //inisialisasi proses memberi isi pada variabel
+    
+final String nama = "Maya";
+       
+        short a = 80;   
+        short b = 90;
+        
+        short c = b;
+        b=a;
+        a=c;  
+        System.out.println(a);
+        System.out.println(b);
+        
+        byte maksimal = -128;
+        byte minimal = 127;
+            System.out.println(128);
+            System.out.println(127+1);            
+    }        
+}

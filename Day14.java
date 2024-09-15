a42c7cedf3033948fd280218b1a7f0d556dcd49a
Mayaismi <mayaismi21@gmail.com>
2024-09-15 19:51:13 +0700

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,16 @@
+public class Day14 {
+    //
+        public static void main(String[] args) {
+        //Main method 
+        
+        long banyak = 2000L;
+        //long adalah tipe data primitif yang menampung angka yang lebih besar
+        //min : 9223372036854775808
+        //max : 9223372036854775807
+        
+            System.out.println("Banyak: " + banyak);
+            //Menampilkan nilai variabel
+            
+        
+    }
+}

fa804ce5c1cce9355ac1864c0ab84c63c03c47c4
Mayaismi <mayaismi21@gmail.com>
2024-09-17 19:46:12 +0700

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,13 @@
+//Bagian class
+    public class Day16 {
+    
+    //method
+    public static void main(String[] args) {
+     
+    //float adalah tipe data primitif yang menyimpan nilai desimal dengan suffix 'f'   
+    float Banyak = 15.000f; 
+    
+    //Menampilkan nilai Variabel
+    System.out.println("Banyak : " + Banyak);
+    }
+}

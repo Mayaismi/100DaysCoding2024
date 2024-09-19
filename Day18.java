108396754172eefaa90608541d6b47e792b6a7fb
Mayaismi <mayaismi21@gmail.com>
2024-09-19 20:14:48 +0700

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,12 @@
+public class Day18 {
+    public static void main(String[] args) {
+        //mendeklarasikan variabel boolean
+        boolean isihati = true;
+        boolean keinginan = false;
+        
+        //mencetak kondisi dan memberikan respons
+        System.out.println("isi hati : " + isihati);
+        System.out.println("keinginan: " + keinginan);
+        
+    }
+}

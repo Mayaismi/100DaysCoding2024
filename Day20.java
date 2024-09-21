d7549010f72b487b1f29acd71680368759f01006
Mayaismi <mayaismi21@gmail.com>
2024-09-21 21:32:08 +0700

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,15 @@
+public class Day20 {
+    public static void main(String[] args) {
+        
+         //Menggunakan tipe data referens Byte
+         Byte angka  = 10;
+         Byte bb     = 38;
+         Byte banyak = 90;
+         
+         //output hasil 
+         System.out.println("Angka      : " + angka);
+         System.out.println("Berat badan: " + bb);
+         System.out.println("Banyak buku: " + banyak);
+    }
+    
+}

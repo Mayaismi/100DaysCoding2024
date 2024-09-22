90046f81b3ebf647048c09967e50f733ae87a5a4
Mayaismi <mayaismi21@gmail.com>
2024-09-22 21:40:15 +0700

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,17 @@
+//Bagian kelas 
+public class Day21 {
+    
+    //Method main
+    public static void main(String[] args) {
+        
+        //Mendeklarasikan variabel bertipe data referens Short
+        Short Tahun = 2005;
+        
+        //Menampilkan isi variabel
+        System.out.println("Tahun lahir Maya: "+ Tahun);
+        
+        
+        
+    }
+    
+}

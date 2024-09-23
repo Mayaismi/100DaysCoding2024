1196ae22c5f4e8f475ec4437a41d147fb4ea2433
Mayaismi <mayaismi21@gmail.com>
2024-09-23 20:24:03 +0700

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,16 @@
+public class Day22 {
+    
+    //Method main
+    public static void main(String[] args) {
+        
+        //Mendeklarasikan variabel dengan tipe data referens Integer
+        Integer Tanggal = 14;
+        Integer Bulan   = 10;
+        
+        //Menampilkan isi variabel
+        System.out.println("Ultah tanggal: " + Tanggal);
+        System.out.println("Bulan        : " +  Bulan);
+        
+    }
+           
+}

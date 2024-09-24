1cf7131d3099469071802e27fdb8aae206128811
Mayaismi <mayaismi21@gmail.com>
2024-09-24 19:08:16 +0700

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,12 @@
+    //Bagian class
+    public class Day23 {
+    
+    //Method main
+    public static void main(String[] args) {
+    
+    //Mendeklarasikan tipe data referens Long
+    Long umur = 18L;
+     
+    //Menampilkan isi Variabel
+    System.out.println("Umur saya : " + umur);;
+    }

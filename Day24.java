35d6e0efd18eb99f0c6c044156480e7ad7792553
Mayaismi <mayaismi21@gmail.com>
2024-09-25 21:35:58 +0700

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,14 @@
+//Bangian class
+public class Day24 {
+    
+//Method main
+public static void main(String[] args) {
+        
+//Mendeklarasikan variabel menggunakan tipe data referens Float
+Float Banyak = 50.0F;
+        
+//autput isi variabel
+System.out.println("Banyak: =" + Banyak);
+    }
+    
+}

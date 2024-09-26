35101735cf8e27a3b7187d56bf1ced27ab169286
Mayaismi <mayaismi21@gmail.com>
2024-09-26 20:45:02 +0700

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,12 @@
+public class Day25 {
+//
+//Method main    
+public static void main (String[] args ){
+    
+//Mendeklarasikan variabel bertipe data referens Double
+Double angka = 37.7;
+
+//Menampilkan isi variabel        
+System.out.println("Angka: " + angka);
+    }
+}

d7e6253aa57e312c8a43f713f5d51eb6b5ac8f74
Mayaismi <mayaismi21@gmail.com>
2024-09-27 21:54:21 +0700

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,14 @@
+public class Day26{
+    
+    //method main
+    public static void main (String[] args){
+        
+        //Mendeklarasikan variabel bertipe data referens Character
+        Character t = 'p';
+        
+        //Menampilkan isi variabel
+        System.out.println("T: " + t);
+        
+        
+    }
+}

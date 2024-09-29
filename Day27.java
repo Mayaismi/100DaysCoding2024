cb4505add5d7a26632605e1ccd1ecdc1242a6ed1
Mayaismi <mayaismi21@gmail.com>
2024-09-29 21:43:34 +0700

    Create Day27.java

---
+++ Day27.java
@@ -0,0 +1,15 @@
+public class Day27 {
+    
+//method main
+public static void main(String[] args) {
+    
+//Mendeklarasikan variabel dengan tipe data referens Boolean    
+Boolean Boleh = true;
+Boolean Tidak = false;
+
+//Menampilkan isi variabel
+System.out.println("Boleh: " + Boleh);
+System.out.println("Tidak: " + Tidak);
+        
+    }
+}

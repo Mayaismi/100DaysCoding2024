639324dcf6d4a33bf53c94449be18ada9dd76c51
Mayaismi <mayaismi21@gmail.com>
2024-10-01 20:55:35 +0700

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+
+public class Day30 {
+public static void main(String[] args) {
+BigDecimal Angka = new BigDecimal ("14");
+    System.out.println("Angka: " + Angka);
+        
+    }
+    
+}

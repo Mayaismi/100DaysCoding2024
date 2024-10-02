cc4c402f9ccecfa9dacefd1357be13843a2ab314
Mayaismi <mayaismi21@gmail.com>
2024-10-02 20:58:45 +0700

    Update Day31.java

--- Day31.java
+++ Day31.java
@@ -3,12 +3,34 @@ public class Day31 {
         String ac = "14";
         int ab = Integer.parseInt(ac);
         System.out.println(ab);
+        
         String a = "10";
         short b = Short.parseShort(a);
         System.out.println(b);
+        
         String may = "2005";
         long maya = Long.parseLong(may);
         System.out.println(maya);
         
+        String c = "10";
+        float h = Float.parseFloat(c);
+        System.out.println(h);
+        
+        String i ="20";
+        double n = Double.parseDouble(i);
+        System.out.println(n);
+        
+        String y = "30";
+        byte k = Byte.parseByte(y);
+        System.out.println(k);
+        
+        String m = "false";
+        boolean boo = Boolean.parseBoolean(m);
+        System.out.println(boo);
+        
+        String konvert = "maya";
+        char z = konvert.charAt(0);
+        System.out.println(z);
     }
 }
+

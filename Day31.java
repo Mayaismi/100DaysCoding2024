6d3f2a03343b747a6e844951d8fe48b6e829132c
Mayaismi <mayaismi21@gmail.com>
2024-10-02 09:03:45 +0700

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,14 @@
+public class Day31 {
+    public static void main(String[] args) {
+        String ac = "14";
+        int ab = Integer.parseInt(ac);
+        System.out.println(ab);
+        String a = "10";
+        short b = Short.parseShort(a);
+        System.out.println(b);
+        String may = "2005";
+        long maya = Long.parseLong(may);
+        System.out.println(maya);
+        
+    }
+}

1d23a17f5dee3b6bb1f6f72596fe4d963acdf773
Mayaismi <mayaismi21@gmail.com>
2024-10-03 20:26:25 +0700

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,40 @@
+public class Day32 {
+public static void main(String[] args) {
+  
+//Bilangan bulat
+int a = 10;
+String b = Integer.toString(a);
+System.out.println(b);
+
+byte c = 20; 
+String d = Byte.toString(c);
+System.out.println(d);
+
+short e = 30;
+String f = Short.toString(e);
+System.out.println(f);
+
+long g = 40;
+String h = Long.toString(g);
+System.out.println(h);
+
+//Bilangan desimal
+float i = 50;
+String j = String.valueOf(i);
+System.out.println(j);
+
+double k = 60;
+String l = String.valueOf(k);
+System.out.println(j);
+
+//Charakter
+char m = 'M';
+String n = m + "";
+System.out.println(n);
+
+boolean o = true;
+String p = o + "";
+System.out.println(p);
+
+    }
+}

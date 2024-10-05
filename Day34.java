d2f93d7cef2c0ae39acb52c4870bdee83cb32aea
Mayaismi <mayaismi21@gmail.com>
2024-10-05 18:43:08 +0700

    Update Day34.java

--- Day34.java
+++ Day34.java
@@ -4,19 +4,31 @@ import java.util.Scanner;
 public class Day034 {
     public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
-    System.out.println("Masukkan nama: ");
-    String nama = sc.nextLine();
-    System.out.println("Masukkan umur anda");
-    int umur = sc.nextInt();
-    System.out.println("Masukkan kelas anda");   
-    char kelas = sc.next().charAt(0);
-    System.out.println("Masukkan status anda");
-    boolean status = sc.nextBoolean();
-    
+        System.out.print("masukkan nama : ");
+        String nama = sc.next();
+        System.out.print("Masukkan nim : ");
+        String nim = sc.next();
+        System.out.print("masukkan umur : ");
+        int umur = sc.nextInt();
+        System.out.print("masukkan alamat : ");
+        String alamat = sc.next();
+        System.out.print("masukkan kelas : ");
+        char kelas = sc.next().charAt(0);
+        System.out.print("masukkan bb : ");
+        double bb = sc.nextDouble();
+        System.out.print("masukkan tb : ");
+        double tb = sc.nextFloat();
+        System.out.println("masukkan pesan dan kesan : ");
+        String pk = sc.next();
+   
     System.out.println("Nama  : " + nama );
+    System.out.println("NIM  : " + nim );
     System.out.println("Umur  : " + umur );
+    System.out.println("Alamat : " + alamat );
     System.out.println("Kelas : " + kelas );
-    System.out.println("Status: " + status );
+    System.out.println("Berat Badan : " + bb  );
+    System.out.println("Tinggi badan : " + tb  );
+    System.out.println("pesan dan kesan : " + pk );
        
         
         

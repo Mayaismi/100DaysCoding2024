b62d3a077208cadbff0d33ddbb7a9d52e85499b4
Mayaismi <mayaismi21@gmail.com>
2024-10-06 18:03:45 +0700

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,19 @@
+public class Day35 {
+    public static void main(String[] args) {
+        //Operator aritmatika: penjumlahan & pengurangan
+        
+        //Denontasi operator aritmatika penjumlahan dan pengurangan
+        int a = 90;
+        int b = 10;
+        
+        //penjumlahan
+        int hasilPenjumlahan = a+b;
+        System.out.println("Hasil penjumlahan : " + a + "+" + b + "=" + hasilPenjumlahan );
+        //pengurangan 
+        int hasilPengurangan = a-b;
+        System.out.println("Hasil penjumlahan : " + a + "-" + b + "=" + hasilPengurangan);
+        
+        
+    }
+    
+}

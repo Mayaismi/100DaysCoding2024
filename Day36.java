aaa5eb3f572acdc1de7c11fcb5b8a939e327af9a
Mayaismi <mayaismi21@gmail.com>
2024-10-07 18:29:11 +0700

    Create Day36.java

---
+++ Day36.java
@@ -0,0 +1,19 @@
+public class Day36 {
+        public static void main(String[] args) {
+        
+            // operator aritmatika: perkalian, pembagian serta modulo
+            int a = 14;
+            int b = 10;
+            
+            int Perkalian = a*b;
+            System.out.println("Perkalian: " + Perkalian );
+            
+            int Pembagian = a/b;
+            System.out.println("Pembagian: " + Pembagian);
+            
+            int Modulo = a%b;
+            System.out.println("Modulo: "+ Modulo);
+        }
+         
+    
+}

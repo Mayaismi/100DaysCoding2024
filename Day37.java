b0987459df2dd0437c1507990b2a662dab5af3af
Mayaismi <mayaismi21@gmail.com>
2024-10-08 18:57:33 +0700

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,20 @@
+public class Day37 {
+    public static void main(String[] args) {
+        
+        //Operator penugasan: pengisian nilai,penambahan dan pengurangan
+        int M = 10;
+        System.out.println("Hasil pengisian: " + M);
+        
+        //penambahan
+        M +=2;
+        System.out.println("Hasil tambah: " +M);
+        
+        //pengurangan
+        M -=5;
+        System.out.println("Hasil kali: " + M);
+        
+        
+        
+    }
+    
+}

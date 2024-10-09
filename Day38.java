bda52b14ac4b74a996768253508f754c479216f5
Mayaismi <mayaismi21@gmail.com>
2024-10-09 19:24:20 +0700

    Create Day38.java

---
+++ Day38.java
@@ -0,0 +1,18 @@
+public class Day38 {
+    public static void main(String[] args) {
+        //Operator penugasan: pengisian,dan perkalian,pengisian dan pembagi
+        int N = 10;
+        System.out.println("Hasil pengisian: " + N);
+        
+        //perkalian
+        N *=2;
+        System.out.println("Hasil kali: " +N);
+        
+        int L =20;
+        //pembagi
+        L /=4;
+        System.out.println("Hasil bagi: " + N);
+        
+    }
+    
+}

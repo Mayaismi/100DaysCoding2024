5bcc366015aba195149b3aea415edd02bd2630eb
Mayaismi <mayaismi21@gmail.com>
2024-10-10 21:23:08 +0700

    Create Day39.java

---
+++ Day39.java
@@ -0,0 +1,12 @@
+public class Day39 {
+    public static void main(String[] args) {
+        //operator penugasan: pengisian dan modulo
+        int nilai = 55;
+        System.out.println("Hasil pengisian: " + nilai);
+        
+        //modulo (sisa bagi)
+        nilai %=4;
+        System.out.println("Hasil modulo: " + nilai);
+    }
+    
+}

6908dbfccbdf7a299a48bd5ea77b0f58ad27d3d4
Mayaismi <mayaismi21@gmail.com>
2024-10-11 21:53:19 +0800

    Create Day40.java

---
+++ Day40.java
@@ -0,0 +1,14 @@
+public class day40 {
+    public static void main(String[]args){
+    int a = 27;
+    int b = 30;
+        
+//        percabangan 
+        
+        if (a < b){
+            System.out.print("nilai a lebih kecil dari nilai b");
+        }else if ( a > b){
+            System.out.println("nilai a lebih besar dari nilai b");
+        }
+    }
+}

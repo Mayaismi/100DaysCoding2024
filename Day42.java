fe3d674d96e4a72afc067dc748c98093a84f8c69
Mayaismi <mayaismi21@gmail.com>
2024-10-13 20:09:47 +0700

    Create Day42.java

---
+++ Day42.java
@@ -0,0 +1,15 @@
+public class Day42 {
+    public static void main(String[] args) {
+        //operator perbandingan: sama dengan dan tidak sama dengan
+    int a = 10;
+    int b = 10;
+        //sama dengan
+    boolean A = a==b;
+    System.out.println("a sama dengan b: " + A);
+    
+        //tidak sama dengan
+    boolean B = a!=b;
+    System.out.println("a tidak sama dengan b :" + B);
+    
+}
+}

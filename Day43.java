db4465f1616d4ff633b27b17554bd1c63e51d54d
Mayaismi <mayaismi21@gmail.com>
2024-10-14 20:43:50 +0700

    Create Day43.java

---
+++ Day43.java
@@ -0,0 +1,19 @@
+public class Day43 {
+    public static void main(String[] args) {
+        //operator perbandingan: lebih besar sama dengan dan lebih kecil sama dengan
+    int a = 14;
+    int b = 15;
+    
+    //lebih besar sama dengan
+    boolean A = a>=b;
+    System.out.println("a lebih besar sama dengan b: " + A);
+    
+    //lebih kecil sama dengan
+    boolean B = a<=b;
+    System.out.println("a lebih kecil sama dengan b: " + B);
+    
+    
+            
+    }
+    
+}

3e7b2901a1943589b7dce0033672346b81729d74
Mayaismi <mayaismi21@gmail.com>
2024-10-15 19:12:22 +0700

    Create Day44.java

---
+++ Day44.java
@@ -0,0 +1,11 @@
+public class Day44 {
+    public static void main(String[] args) {
+        //operator logika: AND
+        int a = 50;
+        int b = 20;
+        int c = 10;
+        System.out.println(a>b && c<b);
+                
+    }
+    
+}

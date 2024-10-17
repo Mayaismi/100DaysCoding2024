f3443084ec12f550930e6bfdf95319d95f0edc63
Mayaismi <mayaismi21@gmail.com>
2024-10-17 19:50:17 +0700

    Create Day46.java

---
+++ Day46.java
@@ -0,0 +1,10 @@
+public class Day46 {
+    public static void main(String[] args) {
+        
+        //operator logika: Negasi/kebalikan
+    boolean nilai = true;
+    boolean nilai2 = false;
+        System.out.println(!nilai);
+        System.out.println(!nilai2);
+    }
+}

4b516a78e3e2981d0e657fdc6db381162c048808
Mayaismi <mayaismi21@gmail.com>
2024-10-18 22:25:34 +0800

    Create Day47.java

---
+++ Day47.java
@@ -0,0 +1,13 @@
+public class Day47 {
+    public static void main(String[] args) {
+        //Percabangan: if
+        int a = 50;
+        int b = 60;
+        boolean c = a<b;
+        
+        if (a<b){
+        }
+        System.out.println("Maya");
+    }
+    
+}

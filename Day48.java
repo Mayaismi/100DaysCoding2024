6e8ea79987a8d8a24a9bb94cff347ab5330f4875
Mayaismi <mayaismi21@gmail.com>
2024-10-19 21:28:38 +0700

    Create Day48.java

---
+++ Day48.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Percabangan: if else
+public class Day48 {
+    public static void main(String[] args) {
+        Scanner maya = new Scanner (System.in);
+        System.out.println("next");
+        int a = maya.nextInt();
+        
+        if (a<50){
+            System.out.println("Ok"); 
+        }
+            else{
+            System.out.println("Y");
+        }
+        }
+        }

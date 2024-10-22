a7ae9c0ecc8e221e12265cf57cc845e821635b2b
Mayaismi <mayaismi21@gmail.com>
2024-10-22 20:39:42 +0700

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,37 @@
+public class Day51 {
+    public static void main(String[] args) {
+        
+        //percabangan SWITCH CASE
+      Scanner maya = new Scanner (System.in);
+      int hari = maya.nextInt();
+      switch (hari){
+          
+          case 1 :
+          System.out.println("M");
+          break;
+          
+          case 2 :
+          System.out.println("A");
+          break;
+          
+          case 3 :
+          System.out.println("Y");
+          break;
+          
+          case 4 :
+          System.out.println("A");
+          break;
+          
+          
+          default:
+          System.out.println("G");
+                  
+          
+              
+      
+      
+      }
+      
+    }
+    
+}

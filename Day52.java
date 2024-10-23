b6b12580d1635c5d53d414256f0eda7f52d1ecb7
Mayaismi <mayaismi21@gmail.com>
2024-10-23 21:19:30 +0700

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+public class Day52 {
+    public static void main(String[] args) {
+        //Ternary
+        int c = 80;
+        String angka = (c<=100)?
+        "Y":"G";
+        System.out.println(angka);       
+    
+}
+    
+}

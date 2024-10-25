94308a305f012b5313f27caf356d6a82251ce584
Mayaismi <mayaismi21@gmail.com>
2024-10-25 19:53:50 +0700

    Create Day54.java

---
+++ Day54.java
@@ -0,0 +1,9 @@
+
+public class Day54 {
+    public static void main(String[] args) {
+        //for Increment
+        for (int a=5; a<=50; a+=3){
+            System.out.println(a);
+    }
+    }
+}

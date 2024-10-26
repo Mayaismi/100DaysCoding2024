bf0fb4ae1a9caa3013db74beccd0a876226deb69
Mayaismi <mayaismi21@gmail.com>
2024-10-26 15:52:25 +0700

    Create Day55.java

---
+++ Day55.java
@@ -0,0 +1,10 @@
+public class Day55 {
+    public static void main(String[] args) {
+        //for Decrement
+        for (int a=10; a<=50; a-=2){
+            System.out.println(a);
+        }
+    }
+            
+    
+}

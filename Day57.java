599d0471682b39e556beaad677cb86d05d092e0f
Mayaismi <mayaismi21@gmail.com>
2024-10-28 19:45:05 +0700

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,10 @@
+public class Day57 {
+    public static void main(String[] args) {
+        //while: while Increment
+        int a = 30;
+        String nama = "Maya";
+        while (a<=60){
+            System.out.println(nama+a++);
+    }
+    }
+}

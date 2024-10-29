81ed4048fcfada15b2615e538164861d496e723f
Mayaismi <mayaismi21@gmail.com>
2024-10-29 14:31:25 +0700

    Create Day58.java

---
+++ Day58.java
@@ -0,0 +1,11 @@
+public class Day58 {
+    public static void main(String[] args) {
+        //looping whileL: Decrement
+        int a = 1;
+        while (a<=6){
+            System.out.println(a);
+            a++;
+        }
+    }
+    
+}

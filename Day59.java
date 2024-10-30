e02b2d3ee405b878de06fc6aacf1343e7132407e
Mayaismi <mayaismi21@gmail.com>
2024-10-30 19:55:47 +0700

    Create Day59.java

---
+++ Day59.java
@@ -0,0 +1,12 @@
+public class Day59 {
+    public static void main(String[] args) {
+        //looping: For Each
+     String nama []  = {"M","A","Y","A"};
+     
+       for (String M : nama){
+           System.out.println(M+"");
+       }
+        
+    }
+    
+}

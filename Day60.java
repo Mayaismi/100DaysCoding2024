c376af06b94d89d4a41ca7603a9db0d9399ca70f
Mayaismi <mayaismi21@gmail.com>
2024-10-31 21:03:12 +0700

    Create Day60.java

---
+++ Day60.java
@@ -0,0 +1,12 @@
+public class Day60 {
+    public static void main(String[] args) {
+        //looping: do while
+        int a = 5;
+        do{
+            System.out.println(a);
+            a+=2;
+        }while (a<=15);
+        
+            }
+    
+}

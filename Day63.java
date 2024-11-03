270db91f1386f765e39aea965ed8bbb68a0855cb
Mayaismi <mayaismi21@gmail.com>
2024-11-03 20:54:42 +0700

    Create Day63.java

---
+++ Day63.java
@@ -0,0 +1,17 @@
+
+public class Day63 {
+    public static void main(String[] args) {
+        //Keyword : continue
+         for (int a = 1; a<=10; a++){
+             if (a%2==0){
+                 continue;
+             }
+              System.out.println(a);               
+             
+         }
+         
+        
+    }
+    
+    
+}

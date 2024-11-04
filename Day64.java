0a1962bdd693127abeba92f482d79157c9906a11
Mayaismi <mayaismi21@gmail.com>
2024-11-04 21:57:10 +0800

    Create Day64.java

---
+++ Day64.java
@@ -0,0 +1,12 @@
+public class Days64 {
+    public static void main(String[] args) {
+        Maya:
+        for( int M=5; M <= 10; M++){
+            if (M/2==0){
+                break Maya;
+                
+        }System.out.println(M);
+        }
+        
+    }
+}

3a9ece90c358b64c65a9732ca50183bfb19a69ef
Mayaismi <mayaismi21@gmail.com>
2024-11-05 20:16:50 +0700

    Create Day65.java

---
+++ Day65.java
@@ -0,0 +1,15 @@
+public class Day65 {
+    public static void main(String[] args) {
+        //Nested loop
+        for (int a=1; a<=10; a++){
+            for(int i=1; i<=10; i++){
+                if (i==2){
+                    System.out.println("i: " + i + " ,a: " + a);
+                    
+                }
+                
+            }
+        }
+    }
+    
+}

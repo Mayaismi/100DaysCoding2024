bf991ece9f50837f76a0e3cf4406a66e0ee95d56
Mayaismi <mayaismi21@gmail.com>
2024-11-07 23:03:02 +0700

    Update Day67.java

--- Day67.java
+++ Day67.java
@@ -1,12 +1,9 @@
 public class Day67 {
-     public void cetakpesan() {
-        //Method Void
-         System.out.println("Ini adalah methode void");
+    public static void main(String[] args) {
+        Day67 maya = new Day67();
+        maya.maya();
     }
-      public static void main(String[] args) {
-        cetakpesan();
-                
+        void maya(){
+        System.out.println("yahh");
     }
-      
-    
-}
+    }

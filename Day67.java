10067fad75ec1632c534e10179ece4fcb1799239
Mayaismi <mayaismi21@gmail.com>
2024-11-07 21:10:03 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,12 @@
+public class Day67 {
+     public static void cetakpesan() {
+        //Method Void
+         System.out.println("Ini adalah methode void");
+    }
+      public static void main(String[] args) {
+        cetakpesan();
+                
+    }
+      
+    
+}

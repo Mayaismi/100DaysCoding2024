da3c69e8ea6622d13c19ba71b5d281f65b64897e
Mayaismi <mayaismi21@gmail.com>
2024-11-08 18:31:27 +0800

    Create Day68.java

---
+++ Day68.java
@@ -0,0 +1,9 @@
+public class Day68 {
+    public static void main(String[] args) {
+       Day68 maya = new Day68();
+        System.out.println(maya.nama());
+    }
+     String nama(){
+        return "maya";
+    } 
+    }

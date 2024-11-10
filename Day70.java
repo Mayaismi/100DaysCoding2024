5ffa0cd830bba6524114aa1294198841e4ce8acf
Mayaismi <mayaismi21@gmail.com>
2024-11-10 21:12:52 +0700

    Create Day70.java

---
+++ Day70.java
@@ -0,0 +1,17 @@
+public class Day70 {
+    public static void main(String[] args) {
+        
+        int angka = 50;
+        int nilai = 45;
+
+        kurang(angka, nilai);
+    }
+
+    static void kurang(int angka, int nilai) {
+        int kurang = angka - nilai;
+        System.out.println(kurang);
+    }
+}
+    
+    
+

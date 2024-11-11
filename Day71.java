1594e96aa9f9f1f758374d527dbf74fc10bca05e
Mayaismi <mayaismi21@gmail.com>
2024-11-11 22:58:04 +0800

    Create Day71.java

---
+++ Day71.java
@@ -0,0 +1,10 @@
+public class Day71 {
+    public static void main(String[] args) {
+        System.out.println("Hellow maya");
+        System.out.println(maya(5+6));
+    }
+    public static int maya(int a) {
+        return a;
+    }
+    
+}

11039bf1258f51ac17140fe13e6621831636e887
Mayaismi <mayaismi21@gmail.com>
2024-11-13 20:52:08 +0700

    Create Day73.java

---
+++ Day73.java
@@ -0,0 +1,9 @@
+public class Day73 {
+    public static void main(String[] args) {
+        String nama = "Maya";
+        
+        System.out.println(nama);
+        System.out.println("cetak indeks ke-3: " + nama.charAt(3));
+    }
+    
+}

1053611757c791465aaec90176857bd4f59a8978
Mayaismi <mayaismi21@gmail.com>
2024-11-14 21:43:42 +0700

    Create Day74.java

---
+++ Day74.java
@@ -0,0 +1,9 @@
+
+public class Day74 {
+    public static void main(String[] args) {
+         String nama = "Maya";
+        
+        System.out.println(nama);
+        System.out.println("jumlah karakter: " + nama.length());
+    }
+}

dad920aa6ed065875d45ce1572819a6dacb4833a
Mayaismi <mayaismi21@gmail.com>
2024-11-19 20:51:05 +0700

    Create Day79.java

---
+++ Day79.java
@@ -0,0 +1,15 @@
+public class Day79 {
+    public static void main(String[] args) {
+        int a = 79;
+        String b = Integer.toString(a);
+        System.out.println("Nilai a dalam bentuk string: " + b);
+        
+        if (a > 30) {
+            System.out.println("Nilai a lebih besar dari 30.");
+        } else {
+            System.out.println("Nilai a kurang dari atau sama dengan 30.");
+        }
+        
+    }
+ 
+}

c10f97ed64168ea5b9e65300f690157c0fa0ac8f
Mayaismi <mayaismi21@gmail.com>
2024-11-20 19:19:31 +0700

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Day80 {
+    public static void main(String[] args) {
+        String a = " Halo Nama Saya Maya ";
+        String b = a.trim();
+        
+        System.out.println("asli: " + a);
+        System.out.println("trim :" + b);
+    }
+}

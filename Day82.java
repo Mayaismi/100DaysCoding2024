552999eaf051274eea5c34d67cf9de4e1e4dca89
Mayaismi <mayaismi21@gmail.com>
2024-11-21 23:40:29 +0700

    Create Day82.java

---
+++ Day82.java
@@ -0,0 +1,14 @@
+public class Day82 {
+    
+     public static void main(String[] args) {
+        double a = 15.5;
+        double b = 5.5;
+        double c = 10.2;
+        
+        System.out.println("nilai math "+a+" = "+(Math.ceil(a)));
+        System.out.println("nilai math "+b+" = "+(Math.floor(b)));
+        System.out.println("nilai math "+c+" = "+(Math.round(c)));
+        
+     
+     }
+}

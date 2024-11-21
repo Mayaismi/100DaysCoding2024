5839949c98f353453cafaf8e0681c881332bb4e1
Mayaismi <mayaismi21@gmail.com>
2024-11-21 22:01:50 +0700

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,10 @@
+public class Day81 {
+    public static void main(String[] args) {
+       String a = "150";  
+        int b = 0;
+        for (int i = 0; i < 1; i++) {
+            b = Integer.parseInt(a);
+        }
+        System.out.println("Angka yang dimasukkan adalah: " + b); 
+    }
+}

abcdafe84078f879a57a9f58cd73b41da3cc464a
Mayaismi <mayaismi21@gmail.com>
2024-11-25 21:53:12 +0800

    Create Day85.java

---
+++ Day85.java
@@ -0,0 +1,15 @@
+public class Day85 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int array = sc.nextInt();
+        int a [] = new int[5];
+        a[0]= 6;
+        a[1]= 7;
+        a[2]= 8;
+        a[3]= 9;
+        a[4]= 10;
+        
+        System.out.println(a[array]);
+    }
+    
+}

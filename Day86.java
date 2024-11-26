c261586bbe1122446c9a2aa717b1d013477e6901
Mayaismi <mayaismi21@gmail.com>
2024-11-26 21:16:10 +0800

    Create Day86.java

---
+++ Day86.java
@@ -0,0 +1,9 @@
+public class Day86 {
+    public static void main(String[] args) {
+        int array[] = {12,13,14,15,11};
+        
+        for (int i = 0; i < array.length ; i++) {
+            System.out.println("indeks ke["+i+"]: "+array[i]);
+        }
+    }
+}

0fcff1cc803850a5150ed01f92d329496a793c8e
Mayaismi <mayaismi21@gmail.com>
2024-12-01 20:43:41 +0800

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,21 @@
+package Day91;
+
+import java.util.Scanner;
+
+
+public class java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        while(true){
+       
+        int angka = sc.nextInt();
+        if ( angka % 2 == 0){
+            System.out.println("angka genap");
+
+        } else{
+            System.out.println("you and i ,end");
+            break;
+        }
+        }
+        }
+}

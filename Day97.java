c24437527eafcef5e63ab1f063013465b76f98fb
Mayaismi <mayaismi21@gmail.com>
2024-12-07 23:52:07 +0800

    Update Day97.java

--- Day97.java
+++ Day97.java
@@ -1,26 +1,26 @@
 public class Day97 {
     public static void main(String[] args) {
-        int batasAwal = 1; 
-        int batasAkhir = 20; 
+        int batasAwal = 1;
+        int batasAkhir = 20;
 
         System.out.println("Bilangan prima antara " + batasAwal + " dan " + batasAkhir + " adalah:");
         for (int i = batasAwal; i <= batasAkhir; i++) {
-            if (isPrima(i)) {
+            if (cekPrima(i)) {
                 System.out.print(i + " ");
             }
         }
     }
 
-    // Fungsi untuk mengecek apakah suatu bilangan prima
-    public static boolean isPrima(int n) {
+    public static boolean cekPrima(int n) {
         if (n <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
-        }
+}
